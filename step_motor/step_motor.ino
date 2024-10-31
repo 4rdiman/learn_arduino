3c97db708821662e180980820946d4fec30f6396
Ardiman <ardim.1775@gmail.com>
2024-10-31 19:29:22 +0700

    moving the step motor using ultrasonic sensor, 1 function

--- step_motor/step_motor.ino
+++ step_motor/step_motor.ino
@@ -1,83 +1,164 @@
+// ultrasonic sensors
+#define trigPin 7
+#define echoPin 6
+
+// step motor
 #define IN1 11
 #define IN2 10
 #define IN3 9
 #define IN4 8
 
+int delay2 = 2;
+int delay6 = 6;
+int delay10 = 10;
+
 void setup() {
-  // Mengatur pin sebagai output
+  Serial.begin(9600);
+  // Mengatur pin step motor sebagai output
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  // mengatur ultrasonic sensors
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // Memutar motor searah jarum jam
-  for (int i = 0; i < 256; i++) { // 512 langkah = 1 putaran penuh
+  // establish variables for duration of the ping, and the distance result
+  // in inches and centimeters:
+  long duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  if(cm > 20){
     stepMotor(1);
   }
-  
-  delay(1000); // Jeda 1 detik
-  
-  // Memutar motor berlawanan arah jarum jam
-  for (int i = 0; i < 256; i++) {
-    stepMotor(-1);
+
+  else if(cm <= 20 && cm > 10){
+    stepMotor(2);
   }
-  
-  delay(1000); // Jeda 1 detik
+
+  else if(cm <= 10 && cm > 5){
+    stepMotor(3);
+  }
+
+  else if(cm <= 5 && cm >= 0){
+    stepMotor(4);
+  }
+
 }
 
 void stepMotor(int direction) {
-  // Jika direction = 1, searah jarum jam; jika -1, berlawanan arah
+  
   if (direction == 1) {
-    // Urutan untuk gerakan searah jarum jam
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
-    delay(2);
+    delay(delay2);
     
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
-    delay(2);
+    delay(delay2);
     
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    delay(2);
+    delay(delay2);
     
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
-    delay(2);
-  } else {
-    // Urutan untuk gerakan berlawanan arah jarum jam
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    delay(2);
-    
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    delay(2);
-    
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-    delay(2);
-    
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-    delay(2);
+    delay(delay2);
   }
+
+    else if (direction == 2) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(delay6);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(delay6);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    delay(delay6);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    delay(delay6);
+  }
+
+    else if (direction == 3) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(delay10);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(delay10);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    delay(delay10);
+    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    delay(delay10);
+  }
+
+    else if (direction == 4) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  } 
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
